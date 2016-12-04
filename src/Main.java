8b56821cd8087bc1f3ace3f6d0140f9c0345dfd4
reilikasaks <reilikasaks@gmail.com>
2016-12-04 15:03:30 +0200

    Laevade pommitamine JavaFX

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,90 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
-public class Main {
-	public static void main(String args[]) {
-		System.out.println("Alustame pommitamisega!");
+public class Main extends Application {
+	GridPane laud;
+	int lauaPikkusLaevades = 5;
+	int laevapikkusPx = 50;
+	Stage mainGameStage;
+	Image laevaPilt = new Image ("pirate.png");
+	ImagePattern laevaMuster = new ImagePattern(laevaPilt);
 
-		int [][] lauaAlgSeis = new int[9][9];
-		int [][] lauaSeis = new int[9][9];
-
-		//uus 9x9 lauaAlgSeis
-		//0 - meri
-		//1 - laev
-		//2 - pihta saanud laev
-		//3 - mööda
-
-		for (int i=0; i<9; i++){
-			for (int j=0; j<9; j++){
-				lauaAlgSeis[i][j] = (int)(Math.random()*1.3);
-			}
-		}
-		prindiLaud(lauaSeis);
-
-		while (true) {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Kuhu tahad pommitada? Formaat: y-x");
-			String input = sc.nextLine();
-			String[] xy = input.split("-");
-			int x = Integer.parseInt(xy[1])-1;
-			int y = Integer.parseInt(xy[0])-1;
-
-			int tabamus = lauaAlgSeis[y][x];
-			if (tabamus == 1) {
-				lauaAlgSeis[y][x] = 2;
-				lauaSeis[y][x] = 2;
-				System.out.println("Pihtas!");
-			} else if (tabamus == 0) {
-				System.out.println("Mööda!");
-				lauaSeis[y][x] = 3;
-			} else if (tabamus == 2 || tabamus==3) {
-				System.out.println("Siia sa juba lasid!");
-			} else {
-				System.out.println("ERROR! Kuidas sa üldse siia said!?");
-				break;
-			}
-			prindiLaud(lauaSeis);
-			boolean labi = kasOnMangLabi(lauaAlgSeis);
-			if (labi){
-				System.out.println("Mäng on läbi!");
-				break;
-			}
-		}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		seadistaLava();
+		sisestaLaevad();
+		reageeriKlikile();
 	}
 
-	private static boolean kasOnMangLabi(int[][] laud) {
-		for (int i = 0; i <9 ; i++) {
-			for (int j = 0; j <9 ; j++) {
-				if (laud[j][i]==1){
-					return false;
+	private void reageeriKlikile() {
+		laud.setOnMouseClicked(event -> {
+			Rectangle ruut = (Rectangle) event.getTarget();
+			String tyyp = ruut.getId();
+			if (tyyp.equals("meri")){
+				ruut.setFill(Color.DARKBLUE);
+			} else if (tyyp.equals("laev")){
+				ruut.setFill(laevaMuster);
+				ruut.setId("põhjas");
+			}
+			if (!laevasidOnAlles()){
+				gameover();
+			}
+		});
+	}
+
+	private void gameover() {
+		mainGameStage.close();
+		StackPane stack = new StackPane();
+		Label go = new Label("Võitsid!");
+		stack.getChildren().add(go);
+		Scene scene = new Scene(stack, 300, 50);
+		Stage goStage = new Stage();
+		goStage.setScene(scene);
+		goStage.show();
+	}
+
+	private boolean laevasidOnAlles() {
+		for (Node ruut : laud.getChildren()){
+			if (ruut.getId().equals("laev")){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void sisestaLaevad() {
+		for (int i = 0; i < lauaPikkusLaevades; i++) {
+			for (int j = 0; j < lauaPikkusLaevades; j++) {
+				//siia tulen 9x9 korda
+				Rectangle ruut = new Rectangle(laevapikkusPx, laevapikkusPx);
+				int rand = (int) (Math.random()*1.3);
+				if (rand==1){
+					ruut.setId("laev");
+				} else {
+					ruut.setId("meri");
 				}
+				ruut.setFill(Color.BLUE);
+				laud.add(ruut, i,j);
 			}
 			
 		}
-		return true;
 	}
 
-	public static void prindiLaud(int[][] laud){
-		//public - avalikult kättesaadav
-		//static - lihtsal moel kättesaadav, ei pea uut objekti kutsuma
-		//void - tühjus; tagastab tühjust
-		for (int i = 0; i < 9 ; i++) {
-			System.out.println(Arrays.toString(laud[i]));
-		}
+	private void seadistaLava() {
+		laud = new GridPane();
+		Scene scene = new Scene(laud, lauaPikkusLaevades*laevapikkusPx, lauaPikkusLaevades*laevapikkusPx);
+		mainGameStage = new Stage();
+		scene.setFill(Color.DARKBLUE);
+		mainGameStage.setScene(scene);
+		mainGameStage.show();
 	}
 }
